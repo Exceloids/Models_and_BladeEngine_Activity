962bd6c986e909b6ec22dfc93e63cb24b2a71150
Exceloids <lhorexcelbombarda22@gmail.com>
2024-07-29 22:12:37 +0800

    Generate a migration for the books table

--- database/migrations/2024_07_29_140827_create_books_table.php
+++ database/migrations/2024_07_29_140827_create_books_table.php
@@ -4,23 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBooksTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->string('isbn');
-            $table->string('title');
-            $table->string('author');
-            $table->text('description')->nullable();
-            $table->date('date_published');
             $table->timestamps();
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('books');
     }
-}
+};
